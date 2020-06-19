be3d054315977a82d9ec960725eca127fc2127ea
kdpark <kdpark1623@gmail.com>
2020-06-19 11:45:11 +0900

    host 주소 삭제

--- src/com/daou/pjt2/config/Config.java
+++ src/com/daou/pjt2/config/Config.java
@@ -1,7 +1,7 @@
 package com.daou.pjt2.config;
 
 public class Config {
-	public final static String HOST = "itzy.daouoffice.com";
+	public final static String HOST = "-";
 	public final static int POP3_PORT = 110;
 	public final static int IMAP_PORT = 143;
 	
